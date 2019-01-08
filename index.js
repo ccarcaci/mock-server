48010404b8f29abf945e1148e5db49e947f9f937
Claudio Carcaci <claudio.carcaci@mia-platform.eu>
2019-01-08 19:01:00 +0100

    Add simple integration with MongDB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require("http");
 var https = require("https");
 var fs = require("fs");
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
 var characters = require("./star-wars.json");
 
 const httpsOptions = {
@@ -20,7 +22,10 @@ const routing = (request, response) => {
 	} else if(url === "/characters") {
 		response.writeHead(200, { "Content-Type": "application/json" });
 		response.write(JSON.stringify(characters));
-	}
+	} else if(url === "/demongo") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify(demodata()));
+  }
 
 	response.end();
 }
@@ -30,3 +35,51 @@ var httpsServer = https.createServer(httpsOptions, (req, res) => routing(req, re
 
 httpServer.listen(httpPort, () => { console.log(`HTTP Server on port ${httpPort}`) } );
 httpsServer.listen(httpsPort, () => { console.log(`HTTPS Server on port ${httpsPort}`) });
+
+// Server functions
+
+const demodata = function() {
+  const mongoUrl = "mongodb://localhost:27017";
+  const dbName = "demoproj";
+  const client = new MongoClient(mongoUrl, { useNewUrlParser: true });
+
+  client.connect(function(err) {
+    assert.equal(null, err);
+    console.log("Connected to Mongodb server");
+    
+    const db = client.db(dbName);
+    
+    insertDocuments(db, function() {
+      findDocuments(db, function() {
+        client.close();
+      });
+    });
+  });
+}
+
+const insertDocuments = function(db, callback) {
+  const collection = db.collection("demodoc");
+
+  collection.insertMany([
+    { a : 1 }, { a : 2 }, { a : 3 }
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+
+    console.log("Inserted 3 docs into the collection");
+    callback(result);
+  });
+}
+
+const findDocuments = function(db, callback) {
+  const collection = db.collection("demodoc");
+
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+
+    console.log("Found the records:");
+    console.log(docs);
+    callback(docs);
+  });
+}
