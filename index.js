c35b79c81f0cf7c797d21f7df7910a7d90753bc3
Claudio Carcaci <claudio.carcaci@mia-platform.eu>
2019-03-04 20:58:40 +0100

    Removed Mongo code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var http = require("http")
 var https = require("https")
 var url = require("url")
 var fs = require("fs")
-const MongoClient = require("mongodb").MongoClient
 const assert = require("assert")
 var characters = require("./swagger-specs/star-wars.json")
 
@@ -25,14 +24,6 @@ const routing = (request, response) => {
 		response.writeHead(200, { "Content-Type": "application/json" })
     response.write(JSON.stringify(characters))
     response.end()
-	} else if(action.pathname === "/demongo") {
-    response.writeHead(200, { "Content-Type": "application/json" })
-
-    demodata().then((data) => {
-      response.write(JSON.stringify(data))
-      response.end()
-    }).catch(error =>
-      console.log(error.message))
   } else if(action.pathname === "/parrot") {
     const content = getUrlParam(action.query.split("&"), "content")
 
@@ -49,25 +40,6 @@ httpServer.listen(httpPort, () => { console.log(`HTTP Server on port ${httpPort}
 httpsServer.listen(httpsPort, () => { console.log(`HTTPS Server on port ${httpsPort}`) })
 
 // Server functions
-
-const demodata = function() {
-  const mongoUrl = "mongodb://localhost:27017"
-  
-  return MongoClient.connect(mongoUrl, { useNewUrlParser: true }).then((client) => {
-    console.log("Connected to Mongodb server")
-  
-    var collection = client.db("demoproj").collection("demodoc")
-
-    return collection.insertMany([{ a : 1 }, { a : 2 }, { a : 3 } ]).then((result) => {
-      assert.equal(3, result.result.n)
-      assert.equal(3, result.ops.length)
-
-      console.log("Inserted 3 docs into collection")
-
-      return collection.find({}).toArray()
-    }).catch((error)  => console.log(error.message))
-  }).catch(error => console.log(error.message))
-}
 
 const getUrlParam = function(rawParams, paramName) {
   return rawParams.map(rawParam => rawParam.split("=")).
